5618de2110858dff74d6afbd28ae87897aacb9af
Moritz Jörg <moke@mailbox.org>
2024-09-24 22:58:06 +0200

    Add alpha versioning

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
       fantomas = {
         enable = true;
         name = "Fantomas formatting";
-        entry = "dotnet fantomas";
+        entry = "fantomas";
         files = "(\\.fs$)|(\\.fsx$)";
       };
     };
@@ -20,7 +20,7 @@ let
   pname = "Saturn.OpenTelemetry";
   dotnet-sdk = pkgs.dotnet-sdk_8;
   dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
-  version = "0.0.1";
+  version = "0.1.0-alpha";
   shell = pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
       dotnet-sdk_8
