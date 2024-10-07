220e2e9dec2af84c76ea689d21b74d633a3006f6
Moritz Jorg <moritz.jorg@tromso.serit.no>
2024-10-07 16:06:30 +0200

    Bump version

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
         }:
         let
           pname = "SaturnOpenTelemetry";
-          version = "0.4.0-alpha";
+          version = "0.5.0-alpha";
           dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0_4xx;
           dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
         in
