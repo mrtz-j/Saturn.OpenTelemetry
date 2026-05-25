6f6b3b4d253e3c53a8f5041eb07962ecddad27f0
Moritz Jörg <moritz.jorg@oceanbox.io>
2026-05-25 17:22:06 +0200

    Add garnix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
     pre-commit-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,80 +8,164 @@
   };
 
   outputs =
-    inputs:
-    inputs.parts.lib.mkFlake { inherit inputs; } {
+    {
+      self,
+      nixpkgs,
+      pre-commit-hooks,
+    }:
+    let
       systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      imports = [
-        inputs.pre-commit-hooks.flakeModule
-      ];
-      perSystem =
-        {
-          config,
-          pkgs,
-          ...
-        }:
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+
+      pname = "SaturnOpenTelemetry";
+      version = "0.6.0-alpha";
+
+      mkPackages =
+        system:
         let
-          pname = "SaturnOpenTelemetry";
-          version = "0.6.0-alpha";
+          pkgs = nixpkgs.legacyPackages.${system};
           dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
           dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0;
-          fsharp-analyzers = pkgs.buildDotnetGlobalTool {
-            pname = "fsharp-analyzers";
-            version = "0.35.0";
-            nugetHash = "sha256-GxQR3Fq28cb+akNbzRTav9nhMtayN/0g2d1G6Ml+ck4=";
-          };
+        in
+        import ./nix/packages {
+          inherit
+            pkgs
+            pname
+            version
+            dotnet-sdk
+            dotnet-runtime
+            ;
+        };
+
+      mkFsharpAnalyzers =
+        pkgs:
+        pkgs.buildDotnetGlobalTool {
+          pname = "fsharp-analyzers";
+          version = "0.35.0";
+          nugetHash = "sha256-GxQR3Fq28cb+akNbzRTav9nhMtayN/0g2d1G6Ml+ck4=";
+        };
+    in
+    {
+      packages = forEachSystem mkPackages;
+
+      checks = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          pre-commit = {
-            check.enable = true;
-            settings = {
-              src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-              package = pkgs.prek;
-              # Do not run at pre-commit time
-              default_stages = [
-                "pre-push"
-              ];
-              hooks = {
-                deadnix.enable = true;
-                nixfmt.enable = true;
-                statix.enable = true;
-                fantomas = {
-                  enable = true;
-                  name = "fantomas";
-                  entry = "${pkgs.fantomas}/bin/fantomas src example";
-                  files = "(\\.fs$)|(\\.fsx$)";
-                };
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+            hooks = {
+              deadnix.enable = true;
+              nixfmt.enable = true;
+              statix.enable = true;
+              fantomas = {
+                enable = true;
+                name = "fantomas";
+                entry = "${pkgs.fantomas}/bin/fantomas src example";
+                files = "(\\.fs$)|(\\.fsx$)";
               };
             };
+            default_stages = [ "pre-push" ];
           };
+        }
+      );
 
-          packages = import ./nix/packages {
-            inherit
-              pkgs
-              pname
-              version
+      apps =
+        let
+          mkApps =
+            system:
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+              fsharp-analyzers = mkFsharpAnalyzers pkgs;
+              packages = mkPackages system;
+            in
+            {
+              fsharp-analyzers = {
+                type = "app";
+                program = "${fsharp-analyzers}/bin/fsharp-analyzers";
+              };
+              example = {
+                type = "app";
+                program = "${packages.example}/bin/Example";
+              };
+            };
+
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+          packages = mkPackages "x86_64-linux";
+
+          nugetPublish = pkgs.writeShellApplication {
+            name = "nuget-publish";
+            runtimeInputs = [
+              pkgs.age
               dotnet-sdk
-              dotnet-runtime
-              ;
+            ];
+            text = ''
+              if [ "''${GARNIX_BRANCH:-}" != "main" ]; then
+                echo "Skipping publish on branch ''${GARNIX_BRANCH:-unknown}"
+                exit 0
+              fi
+
+              export DOTNET_ROOT="${dotnet-sdk.unwrapped}/share/dotnet"
+              export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+
+              NUGET_AUTH_TOKEN=$(age --decrypt -i "$GARNIX_ACTION_PRIVATE_KEY_FILE" ${./secrets/nuget-auth-token.age})
+              NUGET_API_KEY=$(age --decrypt -i "$GARNIX_ACTION_PRIVATE_KEY_FILE" ${./secrets/nuget-api-key.age})
+
+              NUPKG=$(find ${packages.saturn-opentelemetry} -name "*.nupkg" | head -1)
+              if [ -z "$NUPKG" ]; then
+                echo "No .nupkg found in ${packages.saturn-opentelemetry}"
+                exit 1
+              fi
+              echo "Publishing $NUPKG"
+
+              dotnet nuget remove source github 2>/dev/null || true
+              dotnet nuget add source \
+                --username mrtz-j \
+                --password "$NUGET_AUTH_TOKEN" \
+                --store-password-in-clear-text \
+                --name github \
+                "https://nuget.pkg.github.com/mrtz-j/index.json"
+
+              dotnet nuget push "$NUPKG" \
+                --api-key "$NUGET_AUTH_TOKEN" \
+                --source github \
+                --skip-duplicate
+
+              dotnet nuget push "$NUPKG" \
+                --api-key "$NUGET_API_KEY" \
+                --source "https://api.nuget.org/v3/index.json" \
+                --skip-duplicate
+            '';
           };
 
-          apps = {
-            fsharp-analyzers = {
+          baseApps = forEachSystem mkApps;
+        in
+        baseApps
+        // {
+          x86_64-linux = baseApps.x86_64-linux // {
+            nuget-publish = {
               type = "app";
-              program = "${fsharp-analyzers}/bin/fsharp-analyzers";
-            };
-            example = {
-              type = "app";
-              program = "${config.packages.example}/bin/Example";
+              program = "${nugetPublish}/bin/nuget-publish";
             };
           };
+        };
 
-          devShells.default = pkgs.mkShell {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+          fsharp-analyzers = mkFsharpAnalyzers pkgs;
+        in
+        {
+          default = pkgs.mkShell {
             buildInputs = [ dotnet-sdk ];
-
             packages = [
               pkgs.svu
               pkgs.fantomas
@@ -93,14 +173,10 @@
               pkgs.nuget-to-json
               fsharp-analyzers
             ];
-
             DOTNET_ROOT = "${dotnet-sdk.unwrapped}/share/dotnet";
-            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
-            shellHook = ''
-              ${config.pre-commit.installationScript}
-            '';
+            shellHook = self.checks.${system}.pre-commit-check.shellHook;
           };
-        };
+        }
+      );
     };
 }
